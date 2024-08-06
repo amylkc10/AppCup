2f33955e58247d409e41c446fddb6b75bb18871e
yana <tharaneeyana@gmail.com>
2024-08-06 14:34:59 +0400

    Floating button

--- medisync/lib/screens/homepage.dart
+++ medisync/lib/screens/homepage.dart
@@ -12,71 +12,87 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      padding: EdgeInsets.only(top: 60),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 15),
-            child: Text("Hello User!",
-                style: TextStyle(fontSize: 35, fontWeight: FontWeight.w500)),
-          ),
-          SizedBox(height: 30),
-          Padding(
-            padding: EdgeInsets.only(left: 15),
-            child: Text(
-              "List of medications:",
-              style: TextStyle(
-                fontSize: 23,
-                fontWeight: FontWeight.w500,
-                color: Colors.black,
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.teal,
+        tooltip: 'Add Medication',
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => MedicationScreen()),
+          );
+        },
+        child: const Icon(Icons.add, color: Colors.white, size: 28),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.only(top: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: Text("Hello User!",
+                  style: TextStyle(fontSize: 35, fontWeight: FontWeight.w500)),
+            ),
+            SizedBox(height: 30),
+            Padding(
+              padding: EdgeInsets.only(left: 15),
+              child: Text(
+                "List of medications:",
+                style: TextStyle(
+                  fontSize: 23,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black,
+                ),
               ),
             ),
-          ),
-          SizedBox(
+            SizedBox(
               height: 800,
               child: ListView.builder(
                 shrinkWrap: true,
                 itemCount: medications.length,
                 itemBuilder: (context, index) {
                   return InkWell(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => MedicationScreen()));
-                      },
-                      child: Container(
-                          margin: EdgeInsets.symmetric(
-                              vertical: 10, horizontal: 15),
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 25, vertical: 25),
-                          decoration: BoxDecoration(
-                            color: Color.fromARGB(100, 0, 128, 128),
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black12,
-                                blurRadius: 4,
-                                spreadRadius: 2,
-                              ),
-                            ],
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MedicationScreen(),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      padding: EdgeInsets.symmetric(horizontal: 25, vertical: 25),
+                      decoration: BoxDecoration(
+                        color: Color.fromARGB(100, 0, 128, 128),
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 4,
+                            spreadRadius: 2,
                           ),
-                          child: Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              medications[index],
-                              style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.black54),
-                            ),
-                          )));
+                        ],
+                      ),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          medications[index],
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.black54,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
                 },
               ),
             ),
-        ],
+          ],
+        ),
       ),
     );
   }
